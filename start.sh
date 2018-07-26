c8b51e201bd0801199cc3916319f95c80ed7edda
William Goodall <wgoodall01@gmail.com>
2018-07-26 14:01:47 -0400

    configurable PORT, HOST, better logging

--- start.sh
+++ start.sh
@@ -1,8 +1,42 @@
 #!/usr/bin/env bash
+set -eo pipefail
+
+# make temp directory
+dir="$(mktemp -d -t "local-80-proxy-XXXXXX")"
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+cleanup(){
+	rm -r "$dir"
+}
+
+trap cleanup EXIT
 
 if ! which nginx >/dev/null; then
 	echo "Error: nginx not installed or not in PATH";
 	exit 1
 fi
 
-nginx -c "$(pwd)/nginx.conf"
+host="$1"
+port="$2"
+
+if [[ "$host" == "" ]]; then
+	cat <<EOF
+Usage: ./start.sh <host> [<port>]
+
+Proxies all traffic
+
+No host specified.
+EOF
+	exit 1
+fi
+
+if [[ "$port" == "" ]]; then
+	port="3000"
+fi
+
+# sed the nginx.conf with variables.
+<"$path/nginx.conf" sed "s|@PORT@|$port|g;s|@HOST@|$host|g;s|@PATH@|$path|g" >"$dir/nginx.conf"
+
+echo "Proxying traffic from localhost:80 to localhost:$port with header 'Host: $host'"
+
+nginx -c "$dir/nginx.conf"
